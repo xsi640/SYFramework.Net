7ad1671dd7132e710cc28240a48d43599fcf3fd1
xsi640 <xsi64@126.com>
2015-01-16 00:00:18 +0800

    导入代码

---
+++ SYFramework.Net/Common/StaticRamdom.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SYFramework.Net.Common
+{
+    public class StaticRamdom
+    {
+        private static Random _Random = null;
+        private static object _Locker = new object();
+
+        public static Random Random
+        {
+            get
+            {
+                if (_Random == null)
+                {
+                    lock (_Locker)
+                    {
+                        if (_Random == null)
+                        {
+                            long tick = DateTime.Now.Ticks;
+                            _Random = new Random((int)(tick & 0xffffffffL) | (int)(tick >> 32));
+                        }
+                    }
+                }
+                return _Random;
+            }
+        }
+
+        public static int Next(int minValue, int maxValue)
+        {
+            return Random.Next(minValue, maxValue);
+        }
+
+        public static int Next(int maxValue)
+        {
+            return Random.Next(maxValue);
+        }
+
+        public static long Next(long minValue, long maxValue)
+        {
+            byte[] buf = new byte[8];
+            Random.NextBytes(buf);
+            long longRand = BitConverter.ToInt64(buf, 0);
+            return (Math.Abs(longRand % (maxValue - minValue)) + minValue);
+        }
+    }
+}
--- SYFramework.Net/Common/UtilHelper.cs
+++ SYFramework.Net/Common/UtilHelper.cs
@@ -7,23 +7,15 @@ namespace SYFramework.Net.Common
 {
     public static class UtilHelper
     {
-        private static Random _Random = null;
-        private static object _Locker = new object();
 
         public static int GetRamdom(int minValue, int maxValue)
         {
-            if (_Random == null)
-            {
-                lock (_Locker)
-                {
-                    if (_Random == null)
-                    {
-                        long tick = DateTime.Now.Ticks;
-                        _Random = new Random((int)(tick & 0xffffffffL) | (int)(tick >> 32));
-                    }
-                }
-            }
-            return _Random.Next(minValue, maxValue);
+            return StaticRamdom.Next(minValue, maxValue);
+        }
+
+        public static long GetRamdom(long minValue, long maxValue)
+        {
+            return StaticRamdom.Next(minValue, maxValue);
         }
     }
 }
