17061999245876be81b03e78f065a6481a4c0066
xsi640 <xsi640@hotmail.com>
2015-01-16 10:03:08 +0800

    增加静态缓存。

---
+++ SYFramework.Net/Cache/IStaticCache.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SYFramework.Net.Cache
+{
+    public interface IStaticCache<TKey, TValue> where TValue : class
+    {
+        /// <summary>
+        /// 已缓存数量
+        /// </summary>
+        int Count { get; }
+        /// <summary>
+        /// 新增缓存
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
+        void Add(TKey key, TValue value);
+        /// <summary>
+        /// 获取所有缓存数据
+        /// </summary>
+        /// <returns></returns>
+        IList<TValue> GetAll();
+        /// <summary>
+        /// 移除
+        /// </summary>
+        /// <param name="key"></param>
+        void Remove(TKey key);
+    }
+}
---
+++ SYFramework.Net/Cache/StaticCache.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SYFramework.Net.Cache
+{
+    public class StaticCache<TKey, TValue> : IStaticCache<TKey, TValue> where TValue : class
+    {
+        private IDictionary<TKey, TValue> _Dictionary = new Dictionary<TKey, TValue>();
+        private object _Locker = new object();
+
+        public int Count
+        {
+            get { return this._Dictionary.Count; }
+        }
+
+        public void Add(TKey key, TValue value)
+        {
+            lock (this._Locker)
+            {
+                if (this._Dictionary.ContainsKey(key))
+                    this._Dictionary.Remove(key);
+                this._Dictionary.Add(key, value);
+            }
+        }
+
+        public IList<TValue> GetAll()
+        {
+            return this._Dictionary.Values.ToList();
+        }
+
+        public void Remove(TKey key)
+        {
+            lock (this._Locker)
+            {
+                if (this._Dictionary.ContainsKey(key))
+                    this._Dictionary.Remove(key);
+            }
+        }
+    }
+}
