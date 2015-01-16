acbd4e2a71681115937737a66b1f690fc8c67b94
xsi640 <xsi640@hotmail.com>
2015-01-16 09:39:25 +0800

    ColorHelper，RegexHelper导入

---
+++ SYFramework.Net/Common/ColorHelper.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Windows.Media;
+
+namespace SYFramework.Net.Common
+{
+    public static class ColorHelper
+    {
+        /// <summary>
+        /// 获得Color对象 包含透明度
+        /// </summary>
+        /// <param name="color"></param>
+        /// <returns></returns>
+        public static Color GetColor(string color)
+        {
+            if (RegexHelper.IsWpfColor(color))
+            {
+                string a = color.Substring(1, 2);
+                string r = color.Substring(3, 2);
+                string g = color.Substring(5, 2);
+                string b = color.Substring(7, 2);
+
+                Color c = new Color();
+                c.A = Byte.Parse(a, NumberStyles.AllowHexSpecifier);
+                c.R = Byte.Parse(r, NumberStyles.AllowHexSpecifier);
+                c.G = Byte.Parse(g, NumberStyles.AllowHexSpecifier);
+                c.B = Byte.Parse(b, NumberStyles.AllowHexSpecifier);
+                return c;
+            }
+            return Color.FromArgb(255, 255, 255, 255);
+        }
+        /// <summary>
+        /// 获得Color对象,无透明度
+        /// </summary>
+        /// <param name="color"></param>
+        /// <returns></returns>
+        public static Color GetColorNoAlpha(string color)
+        {
+            if (RegexHelper.IsHtmlColor(color))
+            {
+                string r = color.Substring(1, 2);
+                string g = color.Substring(3, 2);
+                string b = color.Substring(5, 2);
+
+                Color c = new Color();
+                c.A = Byte.Parse("FF", NumberStyles.AllowHexSpecifier);
+                c.R = Byte.Parse(r, NumberStyles.AllowHexSpecifier);
+                c.G = Byte.Parse(g, NumberStyles.AllowHexSpecifier);
+                c.B = Byte.Parse(b, NumberStyles.AllowHexSpecifier);
+                return c;
+            }
+            return Color.FromArgb(255, 255, 255, 255);
+        }
+        /// <summary>
+        /// 转换DrawingColorToMediaColor颜色
+        /// </summary>
+        /// <param name="color"></param>
+        /// <returns></returns>
+        public static Color ConvertColorByDrawColor(System.Drawing.Color color)
+        {
+            if (color == null)
+                return Colors.Transparent;
+            return Color.FromArgb(color.A, color.R, color.G, color.B);
+        }
+        /// <summary>
+        /// 获得Color颜色，带透明度
+        /// </summary>
+        /// <param name="color"></param>
+        /// <returns></returns>
+        public static string GetStringColor(Color color)
+        {
+            string a = color.A.ToString("X");
+            string r = color.R.ToString("X");
+            string g = color.G.ToString("X");
+            string b = color.B.ToString("X");
+
+            if (a.Length == 1)
+                a = "0" + a;
+            if (r.Length == 1)
+                r = "0" + r;
+            if (g.Length == 1)
+                g = "0" + g;
+            if (b.Length == 1)
+                b = "0" + b;
+            return string.Format("#{0}{1}{2}{3}", a, r, g, b);
+        }
+        /// <summary>
+        /// 获得Color颜色，无透明度
+        /// </summary>
+        /// <param name="color"></param>
+        /// <returns></returns>
+        public static string GetStringColorNoAlpha(Color color)
+        {
+            string r = color.R.ToString("X");
+            string g = color.G.ToString("X");
+            string b = color.B.ToString("X");
+
+            if (r.Length == 1)
+                r = "0" + r;
+            if (g.Length == 1)
+                g = "0" + g;
+            if (b.Length == 1)
+                b = "0" + b;
+            return string.Format("#{0}{1}{2}", r, g, b);
+        }
+    }
+}
---
+++ SYFramework.Net/Common/RegexHelper.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace SYFramework.Net.Common
+{
+    /// <summary>
+    /// 正则表达式辅助类
+    /// </summary>
+    public static class RegexHelper
+    {
+        public const string WPFCOLOR = @"^#?([a-f]|[A-F]|[0-9]){4}(([a-f]|[A-F]|[0-9]){4})?$";
+        public const string HTMLCOLOR = @"^#?([a-f]|[A-F]|[0-9]){3}(([a-f]|[A-F]|[0-9]){3})?$";
+        public const string NUMBER = @"^[0-9]+$";
+        public const string ENGLISH = @"^[A-Za-z0-9]+$";
+        public const string EMAIL = @"^[\w-]+(\.[\w-]+)*@[\w-]+(\.[\w-]+)+$";
+        public const string IP = @"^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$";
+        public const string URL = @"^(http|https|ftp|rtsp|mms):(\/\/|\\\\)[A-Za-z0-9%\-_@]+\.[A-Za-z0-9%\-_@]+[A-Za-z0-9\.\/=\?%\-&_~`@:\+!;]*$";
+        public const string CHINESE = @"^[\u4e00-\u9fa5]{2,}$";
+        public const string URLPORT = @"^(.*)://([0-9,a-z,A-Z,-,.]+):([0-9]+)?";
+        public const string DOMAIN = @"([0-9A-Za-z]{2,}\.[0-9A-Za-z]{2,3}|[0-9A-Za-z]{2,}\.[0-9A-Za-z]{2,3})$";
+        public const string FILENAME = "\\/:*?\"<>|";
+        public const RegexOptions OPTIONS = RegexOptions.IgnoreCase | RegexOptions.Compiled;
+
+        /// <summary>
+        /// 判断是否网页颜色 如:#ffffffff 包含透明度
+        /// </summary>
+        /// <param name="strColor"></param>
+        /// <returns></returns>
+        public static bool IsWpfColor(string str)
+        {
+            if (string.IsNullOrWhiteSpace(str))
+                return false;
+            Regex r = new Regex(WPFCOLOR, OPTIONS);
+            return r.IsMatch(str);
+        }
+        /// <summary>
+        /// 判断是否网页颜色 如:#ffffff 不包含透明度
+        /// </summary>
+        /// <param name="strColor"></param>
+        /// <returns></returns>
+        public static bool IsHtmlColor(string str)
+        {
+            if (string.IsNullOrWhiteSpace(str))
+                return false;
+            Regex r = new Regex(HTMLCOLOR, OPTIONS);
+            return r.IsMatch(str);
+        }
+        /// <summary>
+        /// 判断是否为数组
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public static bool IsNumber(string str)
+        {
+            if (string.IsNullOrWhiteSpace(str))
+                return false;
+            Regex r = new Regex(NUMBER, OPTIONS);
+            return r.IsMatch(str);
+        }
+        /// <summary>
+        /// 判断是否为电子邮件
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public static bool IsEmail(string str)
+        {
+            if (string.IsNullOrWhiteSpace(str))
+                return false;
+            Regex r = new Regex(EMAIL, OPTIONS);
+            return r.IsMatch(str);
+        }
+        /// <summary>
+        /// 判断是否为IP地址
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public static bool IsIP(string str)
+        {
+            if (string.IsNullOrWhiteSpace(str))
+                return false;
+            Regex r = new Regex(IP, OPTIONS);
+            return r.IsMatch(str);
+        }
+        /// <summary>
+        /// 判断是否为URL
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public static bool IsUrl(string str)
+        {
+            if (string.IsNullOrWhiteSpace(str))
+                return false;
+            Regex r = new Regex(URL, OPTIONS);
+            return r.IsMatch(str);
+        }
+        /// <summary>
+        /// 判断是否为中文
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public static bool IsChinese(string str)
+        {
+            if (string.IsNullOrWhiteSpace(str))
+                return false;
+            Regex r = new Regex(CHINESE, OPTIONS);
+            return r.IsMatch(str);
+        }
+        /// <summary>
+        /// 判断是否为英文字母
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public static bool IsAbc(string str)
+        {
+            if (string.IsNullOrWhiteSpace(str))
+                return false;
+            Regex r = new Regex(ENGLISH, OPTIONS);
+            return r.IsMatch(str);
+        }
+        /// <summary>
+        /// 通过URL获得协议、IP地址、端口号
+        /// </summary>
+        /// <param name="str"></param>
+        /// <param name="proctocol"></param>
+        /// <param name="ipaddress"></param>
+        /// <param name="port"></param>
+        public static void ConvertUrl(string str, out string proctocol, out string ipaddress, out int port)
+        {
+            proctocol = string.Empty;
+            ipaddress = string.Empty;
+            port = 0;
+
+            if (Regex.IsMatch(str, URLPORT, OPTIONS))
+            {
+                Match m = Regex.Match(str, URLPORT, OPTIONS);
+                if (m.Groups.Count == 4)
+                {
+                    proctocol = m.Groups[1].Value;
+                    ipaddress = m.Groups[2].Value;
+                    port = TypeParseHelper.StrToInt32(m.Groups[3].Value);
+                }
+            }
+        }
+
+        public static bool IsFileName(string str)
+        {
+            if (!string.IsNullOrEmpty(str))
+            {
+                char[] arr = FILENAME.ToCharArray();
+                char[] array = str.ToCharArray();
+                foreach (char c in array)
+                {
+                    foreach (char ch in arr)
+                    {
+                        if (c == ch)
+                        {
+                            return true;
+                        }
+                    }
+                }
+            }
+            return false;
+        }
+
+        public static bool IsDomain(string str)
+        {
+            if (string.IsNullOrWhiteSpace(str))
+                return false;
+            Regex r = new Regex(DOMAIN, OPTIONS);
+            return r.IsMatch(str);
+        }
+    }
+}
